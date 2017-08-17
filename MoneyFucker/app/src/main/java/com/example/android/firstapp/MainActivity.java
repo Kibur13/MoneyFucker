bd6fec762dfe045cebe4983bbdf1e54f6078884e
Kibur13 <kibur13@gmail.com>
2017-08-16 23:27:49 -0400

    Julian is the Idiot

--- MoneyFucker/app/src/main/java/com/example/android/firstapp/MainActivity.java
+++ MoneyFucker/app/src/main/java/com/example/android/firstapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "No Julian Is The Idiot, Idiot", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
