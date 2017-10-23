ca14e69c29141e07db1f98b4cbaa24382fc8f573
Meeth-PC <j2md711@gmail.com>
2017-10-23 06:33:42 -0400

    Work done at home

--- MoneyMapper/app/src/main/java/com/example/mrclean/moneymapper/Features/AccountAdapterListFragment.java
+++ MoneyMapper/app/src/main/java/com/example/mrclean/moneymapper/Features/AccountAdapterListFragment.java
@@ -1,15 +1,22 @@
 package com.example.mrclean.moneymapper.Features;
 
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.example.mrclean.moneymapper.Accounts.Account;
+import com.example.mrclean.moneymapper.Database.AccountRealmDataMethods;
 import com.example.mrclean.moneymapper.Features.AccountFragment.OnListFragmentInteractionListener;
 import com.example.mrclean.moneymapper.Features.dummy.DummyContent.DummyItem;
+import com.example.mrclean.moneymapper.R;
 
 import java.util.List;
+
+import io.realm.OrderedRealmCollection;
+import io.realm.Realm;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link DummyItem} and makes a call to the
@@ -19,7 +26,14 @@ import java.util.List;
 public class AccountAdapterListFragment extends RecyclerView.Adapter<AccountAdapterListFragment.ViewHolder> {
 
     private final List<DummyItem> mValues;
+
     private final OnListFragmentInteractionListener mListener;
+
+    private RecyclerView accountRecyclerView;
+
+    private AccountAdapter adapt;
+    AccountRealmDataMethods RealmSource;
+
 
     public AccountAdapterListFragment(List<DummyItem> items, OnListFragmentInteractionListener listener) {
         mValues = items;
@@ -28,8 +42,17 @@ public class AccountAdapterListFragment extends RecyclerView.Adapter<AccountAdap
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
+        View view;
+        view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_account, parent, false);
+        AccountRealmDataMethods RealmSource = new AccountRealmDataMethods();
+        RealmSource.open();
+       // AccountAdapter adapt = new AccountAdapter((OrderedRealmCollection<Account>)
+         //       RealmSource.getAllAccounts(),true);
+
+        setupRecyclerView(view);
+
+        RealmSource.close();
         return new ViewHolder(view);
     }
 
@@ -38,6 +61,7 @@ public class AccountAdapterListFragment extends RecyclerView.Adapter<AccountAdap
         holder.mItem = mValues.get(position);
         holder.mIdView.setText(mValues.get(position).id);
         holder.mContentView.setText(mValues.get(position).content);
+
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,5 +97,18 @@ public class AccountAdapterListFragment extends RecyclerView.Adapter<AccountAdap
         public String toString() {
             return super.toString() + " '" + mContentView.getText() + "'";
         }
+    }
+
+    private void setupRecyclerView(View view)
+    {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(view);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        accountRecyclerView.setLayoutManager(layoutManager);
+
+        accountRecyclerView.setHasFixedSize(true);
+
+        adapt = new AccountAdapter((OrderedRealmCollection<Account>)
+                RealmSource.getAllAccounts(),true);
+        accountRecyclerView.setAdapter(adapt);
     }
 }
